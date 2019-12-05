f7c3cc3e4753b9b66d160bb9fc013ba90e2aeb60
gianbrazzini <gianbrazzini@speetra.com>
2019-12-04 21:36:04 -0600

    added package.json and scripts; read setup.py from package.json; build and release scripts created and tested

--- setup.py
+++ setup.py
@@ -2,21 +2,29 @@
 Setup script
 """
 
+import json
+import re
+
 import setuptools
 
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+package = json.load(open('package.json', 'r'))
+author = package['author']
+author_name = re.search(r'<(.*)>', author).groups()[0]
+author_email = re.sub('(<(?:.*)>)', '', author).strip()
+
 setuptools.setup(
-    name='do',
-    version='0.0.1',
-    author='Tim Davis',
-    author_email='author@example.com',
-    description='Standardized data structures for Python',
+    name=package['name'],
+    version=package['version'],
+    author=author_name,
+    author_email=author_email,
+    description=package['description'],
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/timdavis3991/do-py',
+    url='https://github.com/do-py-together/do-py',
     packages=setuptools.find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
