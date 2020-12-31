30a04898e502595cb9a0fe709ec13182ecc3ad6b
Gian Brazzini <gian@pulsem.me>
2020-12-31 15:09:47 -0600

    Updated how package.json and README are read in setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ Setup script
 from __future__ import print_function
 
 import json
+import os
 import re
 
 import setuptools
 
 
-with open('README.md', 'r') as fh:
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(dir_path, 'README.md'), 'r') as fh:
     long_description = fh.read()
 
-package = json.load(open('package.json', 'r'))
+package = json.load(open(os.path.join(dir_path, 'package.json'), 'r'))
 author = package['author']
 author_name = re.sub('(<(?:.*)>)', '', author).strip()
 author_email = re.search(r'<(.*)>', author).groups()[0]
